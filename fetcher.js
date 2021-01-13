c2abfce46cb56631a15f462e1d2e4e71f889727b
DrMustafaH <mustafaharith@gmail.com>
2021-01-13 13:31:39 -0800

    page fetcher is done

---
+++ fetcher.js
@@ -0,0 +1,12 @@
+const myArgs = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs')
+
+
+const byteCounter = request(myArgs[0], (error, response, body) => {
+  const strRes = JSON.stringify(body)
+  const byteCount = strRes.length;
+  fs.writeFile(myArgs[1], strRes, () => {
+    console.log(`Downloaded and saved ${byteCount} bytes to ${myArgs[1]}`);
+  });
+})
